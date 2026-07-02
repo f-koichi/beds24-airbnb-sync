e053e4df657131d4c7c253fd046455a94068cce8
koichi <f.koichi@saco.social>
2026-07-02 17:10:45 +0900

    Extend SYNC_DAYS default from 30 to 365

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BEDS24_API_KEY = os.environ["BEDS24_API_TOKEN"]
 BEDS24_API_BASE = "https://api.beds24.com/json"
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "8080"))
-SYNC_DAYS = int(os.getenv("SYNC_DAYS", "30"))
+SYNC_DAYS = int(os.getenv("SYNC_DAYS", "365"))
 
 ROOM_MAPPING = {
     497200: "Nutshell",
